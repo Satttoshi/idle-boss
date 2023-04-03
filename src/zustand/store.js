14eaa4625c6f7eb9dbb3b44b64d360c1ae118ec7
Josh <joshuastolle445@gmail.com>
2023-04-03 13:01:58 +0200

    write addMoney handler func

--- src/zustand/store.js
+++ src/zustand/store.js
@@ -1,7 +1,8 @@
 import { create as createZustand } from "zustand";
 
 const useStore = createZustand((set) => ({
-  money: 4420692,
+  money: 0,
+  addMoney: () => set((state) => ({ money: state.money + 15 })),
 }));
 
 export default useStore;
