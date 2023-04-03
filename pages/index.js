b28ed3827cd8a337aeeb315ecbc87388ff91c1c0
Josh <joshuastolle445@gmail.com>
2023-04-03 10:37:08 +0200

    display money value formatted

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import MoneyDisplay from "@/src/components/MoneyDisplay";
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
       <MoneyDisplay />
     </div>
   );
--- src/components/MoneyDisplay.js
+++ src/components/MoneyDisplay.js
@@ -3,9 +3,12 @@ import useStore from "@/src/zustand/store";
 export default function MoneyDisplay() {
   const { money } = useStore();
 
+  const moneyString =
+    money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + ",00 €";
+
   return (
     <div>
-      <h1>Money: {money}</h1>
+      <h1>Money: {moneyString}</h1>
     </div>
   );
 }
