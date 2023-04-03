4e993ae7a0304456fa4205455bbc383821890266
Josh <joshuastolle445@gmail.com>
2023-04-03 13:06:48 +0200

    fix test description

--- src/components/MoneyButton.test.js
+++ src/components/MoneyButton.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import MoneyButton from "./MoneyButton";
 
-test("renders money display", () => {
+test("renders money button", () => {
   render(<MoneyButton />);
   const moneyDisplayElement = screen.getByRole("button");
   expect(moneyDisplayElement).toBeInTheDocument();
