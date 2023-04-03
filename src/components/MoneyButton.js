516323dc6d1928a21890fe776344a88b4f21ebc9
Josh <joshuastolle445@gmail.com>
2023-04-03 15:29:51 +0200

    edit moneybutton

--- src/components/MoneyButton.js
+++ src/components/MoneyButton.js
@@ -3,8 +3,21 @@ import useStore from "~/src/zustand/store";
 export default function MoneyButton() {
   const { addMoney, tier } = useStore();
   const { delay } = tier[0];
+
+  let disabled = false;
   return (
-    <button type="button" onClick={addMoney}>
+    <button
+      type="button"
+      disabled={disabled}
+      onClick={() => {
+        disabled = true;
+        addMoney();
+        setTimeout(() => {
+          addMoney();
+          disabled = false;
+        }, delay);
+      }}
+    >
       Click me
     </button>
   );
