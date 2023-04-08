950823d08ee78800fd97fa082b2044358c300b7a
Josh <joshuastolle445@gmail.com>
2023-04-08 13:27:34 +0200

    style heading

--- src/components/MoneyDisplay.js
+++ src/components/MoneyDisplay.js
@@ -11,12 +11,37 @@ export default function MoneyDisplay() {
     }) + " €";
 
   return (
-    <div>
-      <StyledHeader>Money: {moneyString}</StyledHeader>
-    </div>
+    <StyledHeader>
+      <div>
+        <h2>Username</h2>
+        <h1>{moneyString}</h1>
+      </div>
+    </StyledHeader>
   );
 }
 
-const StyledHeader = styled.h1`
-  font-family: var(--font1);
+const StyledHeader = styled.header`
+  background-color: var(--5);
+  width: 100%;
+  height: 80px;
+  display: flex;
+  align-items: center;
+
+  div {
+    transform: translateX(100px);
+  }
+
+  h1 {
+    font-family: var(--font2);
+    margin: 0;
+    font-weight: 400;
+    font-size: 1.8rem;
+  }
+
+  h2 {
+    font-family: var(--font1);
+    margin: 0;
+    font-weight: 400;
+    font-size: 0.8rem;
+  }
 `;
