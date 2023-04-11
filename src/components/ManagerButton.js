06b3fb3b427f4973d3658c62784893d3218636cc
Josh <joshuastolle445@gmail.com>
2023-04-11 23:09:30 +0200

    render button in tier component

--- src/components/ManagerButton.js
+++ src/components/ManagerButton.js
@@ -1,4 +1,5 @@
-import { useStore } from "~/src/zustand/store";
+import useStore from "~/src/zustand/store";
+import styled from "styled-components";
 
 export default function ManagerButton({ tier }) {
   const { setTier, clickTimer } = useStore();
@@ -12,12 +13,23 @@ export default function ManagerButton({ tier }) {
   }
 
   return (
-    <button
-      disabled={!tier.hasManager}
+    <StyledButton
+      disabled={tier.hasManager}
       type="button"
       onClick={handleHireManager}
     >
-      Hire Manager for {currentTier.name}
-    </button>
+      Hire Manager
+    </StyledButton>
   );
 }
+
+const StyledButton = styled.button`
+  position: absolute;
+  bottom: 0;
+  left: 90px;
+
+  z-index: 100;
+  width: 50px;
+  height: 30px;
+  font-size: 10px;
+`;
