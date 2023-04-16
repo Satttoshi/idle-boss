c7241bfcce694a48bbbd31b572f8b7e6ca5b2e50
Josh <joshuastolle445@gmail.com>
2023-04-16 08:39:47 +0200

    add bar with function

--- src/components/Milestones.js
+++ src/components/Milestones.js
@@ -4,9 +4,25 @@ export default function Milestones({ investCount, currentMilestone }) {
   return (
     <StyledBox>
       <h3>{investCount + " / " + currentMilestone}</h3>
+      <StyledBar path={getBarClipPath(investCount, currentMilestone)} />
     </StyledBox>
   );
 }
+
+function getBarClipPath(investCount, currentMilestone) {
+  const progress = investCount / currentMilestone;
+  const clipPath = `inset(0px ${62 - progress * 62}% 0px 0px)`;
+  return clipPath;
+}
+
+const StyledBar = styled.div`
+  background-color: var(--4);
+  position: absolute;
+  width: 62px;
+  height: 18px;
+  border-radius: 15px;
+  clip-path: ${({ path }) => path};
+`;
 
 const StyledBox = styled.div`
   background-color: var(--5);
@@ -21,12 +37,13 @@ const StyledBox = styled.div`
   z-index: 15;
   width: 68px;
   height: 24px;
-
   display: grid;
   place-items: center;
 
   h3 {
+    font-weight: 600;
     margin: 0;
+    z-index: 20;
     -webkit-user-select: none;
     -ms-user-select: none;
     user-select: none;
