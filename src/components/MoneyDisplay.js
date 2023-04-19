4988451a8d9978681949454397fbefeac1c8d957
Josh <joshuastolle445@gmail.com>
2023-04-19 11:45:09 +0200

    add dotting to scaled names

--- src/components/MoneyDisplay.js
+++ src/components/MoneyDisplay.js
@@ -18,26 +18,51 @@ export default function MoneyDisplay() {
 
     const moneyString = Math.floor(money).toString();
 
+    function addDots(numberString) {
+      let newString = "";
+      for (let i = 0; i < numberString.length; i++) {
+        if (i % 3 === 0 && i !== 0) {
+          newString = "." + newString;
+        }
+        newString = numberString[numberString.length - i - 1] + newString;
+      }
+      return newString;
+    }
+
     if (moneyString.length > 32) {
-      return moneyString.slice(0, moneyString.length - 30) + " Decillion";
+      return (
+        addDots(moneyString.slice(0, moneyString.length - 30)) + " Decillion"
+      );
     } else if (moneyString.length > 29) {
-      return moneyString.slice(0, moneyString.length - 27) + " Nonillion";
+      return (
+        addDots(moneyString.slice(0, moneyString.length - 27)) + " Nonillion"
+      );
     } else if (moneyString.length > 26) {
-      return moneyString.slice(0, moneyString.length - 24) + " Septillion";
+      return (
+        addDots(moneyString.slice(0, moneyString.length - 24)) + " Septillion"
+      );
     } else if (moneyString.length > 23) {
-      return moneyString.slice(0, moneyString.length - 21) + " Sextillion";
+      return (
+        addDots(moneyString.slice(0, moneyString.length - 21)) + " Sextillion"
+      );
     } else if (moneyString.length > 20) {
-      return moneyString.slice(0, moneyString.length - 18) + " Quintillion";
+      return (
+        addDots(moneyString.slice(0, moneyString.length - 18)) + " Quintillion"
+      );
     } else if (moneyString.length > 17) {
-      return moneyString.slice(0, moneyString.length - 15) + " Quadrillion";
+      return (
+        addDots(moneyString.slice(0, moneyString.length - 15)) + " Quadrillion"
+      );
     } else if (moneyString.length > 14) {
-      return moneyString.slice(0, moneyString.length - 12) + " Trillion";
+      return (
+        addDots(moneyString.slice(0, moneyString.length - 12)) + " Trillion"
+      );
     } else if (moneyString.length > 11) {
-      return moneyString.slice(0, moneyString.length - 9) + " Billion";
+      return addDots(moneyString.slice(0, moneyString.length - 9)) + " Billion";
     } else if (moneyString.length > 8) {
-      return moneyString.slice(0, moneyString.length - 6) + " Million";
+      return addDots(moneyString.slice(0, moneyString.length - 6)) + " Million";
     } else {
-      return moneyString;
+      return addDots(moneyString);
     }
   }
 
