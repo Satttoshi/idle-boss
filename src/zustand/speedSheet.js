41728d101489237404bc18ff2e6abb802df473eb
Josh <joshuastolle445@gmail.com>
2023-04-22 13:15:13 +0200

    add setSpeed function and use as config sheet

---
+++ src/zustand/speedSheet.js
@@ -0,0 +1,70 @@
+export default function setSpeed(tierMilestoneIndex, tierIndex, tierDelay) {
+  switch (tierIndex) {
+    case 1:
+      // Tier 1
+      switch (tierMilestoneIndex) {
+        case 0:
+          return Math.round(tierDelay / Math.sqrt(3));
+        case 1:
+          return Math.round(tierDelay / Math.sqrt(3));
+        case 3:
+          return Math.round(tierDelay / 2);
+        case 4:
+          return Math.round(tierDelay / 10);
+        case 5:
+          return Math.round(tierDelay / 3);
+        default:
+          return Math.round(tierDelay / 2);
+      }
+    case 2:
+      // Tier 2
+      switch (tierMilestoneIndex) {
+        case 0:
+          return Math.round(tierDelay / Math.sqrt(5));
+        case 1:
+          return Math.round(tierDelay / Math.sqrt(5));
+        case 3:
+          return Math.round(tierDelay / 10);
+        case 4:
+          return Math.round(tierDelay / 3);
+        default:
+          return Math.round(tierDelay / 2);
+      }
+    case 3:
+      // Tier 3
+      switch (tierMilestoneIndex) {
+        case 0:
+          return Math.round(tierDelay / Math.sqrt(4));
+        case 1:
+          return Math.round(tierDelay / Math.sqrt(4));
+        case 2:
+          return Math.round(tierDelay / 8);
+        case 3:
+          return Math.round(tierDelay / 4);
+        default:
+          return Math.round(tierDelay / 2);
+      }
+    case 4:
+      // Tier 4
+      switch (tierMilestoneIndex) {
+        case 0:
+          return Math.round(tierDelay / Math.sqrt(2));
+        case 1:
+          return Math.round(tierDelay / Math.sqrt(2));
+        case 3:
+          return Math.round(tierDelay / 3);
+        default:
+          return Math.round(tierDelay / 2);
+      }
+    default:
+      // All other tiers
+      switch (tierMilestoneIndex) {
+        case 0:
+          return Math.round(tierDelay / Math.sqrt(1.5));
+        case 1:
+          return Math.round(tierDelay / Math.sqrt(1.5));
+        default:
+          return Math.round(tierDelay / 2);
+      }
+  }
+}
