a28ae2727ca526f67ed79d55586852965b4a129e
Josh <joshuastolle445@gmail.com>
2023-04-23 11:01:28 +0200

    add flow svgs

---
+++ src/components/ProgressBarAnimation.js
@@ -0,0 +1,13 @@
+import styled, { keyframes } from "styled-components";
+
+export default function ProgressBarAnimation({ isPerSecond }) {
+  return;
+}
+
+const FlowAnimationContainer = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 0px;
+  width: 496px;
+  height: 30px;
+`;
