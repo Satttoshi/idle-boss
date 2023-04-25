e4777027ab8a472d43fe474019c102c17ba50725
Josh <joshuastolle445@gmail.com>
2023-04-25 11:55:46 +0200

    add react-countdown renderer

--- src/components/TierTimer.js
+++ src/components/TierTimer.js
@@ -1,16 +1,59 @@
 import styled from "styled-components";
 import Countdown from "react-countdown";
 
+import useStore from "~/src/zustand/store";
+
+const renderer = ({ days, hours, minutes, seconds, total, completed }) => {
+  console.log(total);
+
+  if (total < 60000) {
+    return <StyledCountdown>0 : {seconds} s</StyledCountdown>;
+  }
+
+  if (total < 3600000) {
+    return (
+      <StyledCountdown>
+        {minutes} : {seconds} m
+      </StyledCountdown>
+    );
+  } else if (total < 86400000) {
+    return (
+      <StyledCountdown>
+        {hours} : {minutes} h
+      </StyledCountdown>
+    );
+  } else if (total < 604800000) {
+    return (
+      <StyledCountdown>
+        {days} : {hours} d
+      </StyledCountdown>
+    );
+  }
+};
 export default function Timer() {
   return (
-    <StyledTimer>
-      <StyledNumber>0</StyledNumber>
-      <StyledSymbol>:</StyledSymbol>
-      <StyledNumber>00</StyledNumber>
-      <StyledSymbol>s</StyledSymbol>
-    </StyledTimer>
+    <>
+      <Countdown date={Date.now() + 6000} renderer={renderer} />
+      <StyledTimer>
+        <StyledNumber>0</StyledNumber>
+        <StyledSymbol>:</StyledSymbol>
+        <StyledNumber>00</StyledNumber>
+        <StyledSymbol>s</StyledSymbol>
+      </StyledTimer>
+    </>
   );
 }
+
+const StyledCountdown = styled.span`
+  position: absolute;
+
+  z-index: 20;
+  top: 100px;
+
+  font-family: var(--font1);
+  font-weight: 500;
+  font-size: 0.8rem;
+`;
 
 const StyledTimer = styled.div`
   position: absolute;
