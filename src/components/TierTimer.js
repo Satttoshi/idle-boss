283e8066b44f38d5a3c4997073cf2cea20ead8b1
Josh <joshuastolle445@gmail.com>
2023-04-25 13:21:13 +0200

    add different countdown solution

--- src/components/TierTimer.js
+++ src/components/TierTimer.js
@@ -1,47 +1,73 @@
 import styled from "styled-components";
 import Countdown from "react-countdown";
+import { useEffect, useState } from "react";
+import useStore from "~/src/zustand/store";
 
-const renderer = ({ days, hours, minutes, seconds, total, completed }) => {
-  function convertTo2Digits(number) {
-    return number < 10 ? `0${number}` : number;
-  }
+// const renderer = ({ days, hours, minutes, seconds, total, completed }) => {
+//   function convertTo2Digits(number) {
+//     return number < 10 ? `0${number}` : number;
+//   }
 
-  if (completed) {
-    return <StyledCountdown>00 : 00 s</StyledCountdown>;
-  }
+//   if (completed) {
+//     return <StyledCountdown>00 : 00 s</StyledCountdown>;
+//   }
 
-  if (total < 60000) {
-    return <StyledCountdown>0 : {convertTo2Digits(seconds)} s</StyledCountdown>;
-  } else if (total < 3600000) {
-    return (
-      <StyledCountdown>
-        {minutes} : {convertTo2Digits(seconds)} s
-      </StyledCountdown>
-    );
-  } else if (total < 86400000) {
-    return (
-      <StyledCountdown>
-        {hours} : {convertTo2Digits(minutes)} m
-      </StyledCountdown>
-    );
-  } else if (total < 604800000) {
-    return (
-      <StyledCountdown>
-        {days} : {convertTo2Digits(hours)} h
-      </StyledCountdown>
-    );
-  }
-};
+//   if (total < 60000) {
+//     return <StyledCountdown>0 : {convertTo2Digits(seconds)} s</StyledCountdown>;
+//   } else if (total < 3600000) {
+//     return (
+//       <StyledCountdown>
+//         {minutes} : {convertTo2Digits(seconds)} m
+//       </StyledCountdown>
+//     );
+//   } else if (total < 86400000) {
+//     return (
+//       <StyledCountdown>
+//         {hours} : {convertTo2Digits(minutes)} h
+//       </StyledCountdown>
+//     );
+//   } else if (total < 604800000) {
+//     return (
+//       <StyledCountdown>
+//         {days} : {convertTo2Digits(hours)} d
+//       </StyledCountdown>
+//     );
+//   }
+// };
+
+// export default function Timer({ delay, isFilling }) {
+//   return (
+//     <StyledTimer>
+//       {isFilling && <Countdown date={Date.now() + delay} renderer={renderer} />}
+//     </StyledTimer>
+//   );
+// }
 
 export default function Timer({ delay, isFilling }) {
+  const [timeRemaining, setTimeRemaining] = useState(Math.floor(delay / 1000));
+
+  useEffect(() => {
+    let intervalId;
+
+    const tick = () => {
+      setTimeRemaining((prevTimeRemaining) => {
+        if (prevTimeRemaining === 0) {
+          clearInterval(intervalId);
+          return 0;
+        }
+        return prevTimeRemaining - 1;
+      });
+    };
+
+    intervalId = setInterval(tick, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [timeRemaining]);
+
   return (
-    <>
-      <StyledTimer>
-        {isFilling ? (
-          <Countdown date={Date.now() + delay} renderer={renderer} />
-        ) : null}
-      </StyledTimer>
-    </>
+    <StyledTimer>
+      {isFilling && <StyledCountdown>{timeRemaining}</StyledCountdown>}
+    </StyledTimer>
   );
 }
 
@@ -50,6 +76,10 @@ const StyledCountdown = styled.span`
   font-weight: 500;
   font-size: 0.8rem;
   word-spacing: -1px;
+
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
 `;
 
 const StyledTimer = styled.div`
@@ -72,17 +102,4 @@ const StyledTimer = styled.div`
   align-items: center;
   justify-content: center;
   gap: 2px;
-`;
-
-const StyledNumber = styled.span`
-  -webkit-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-`;
-const StyledSymbol = styled.span`
-  font-size: 0.7rem;
-  transform: translateY(-3%);
-  -webkit-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
 `;
